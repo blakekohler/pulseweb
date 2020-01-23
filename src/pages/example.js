e0393f0ae12a46dcdaa5e8e90d528177b5be417d
Blake Kohler <blakekohler@Blakes-MacBook-Pro.local>
2020-01-23 16:27:00 -0700

    click through page done

--- src/pages/example.js
+++ src/pages/example.js
@@ -4,6 +4,8 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Container from "../components/contentContainer"
 import logo from "../images/logo.png"
+import showcase from "../images/showcase.jpg"
+import blake from "../images/blake.jpg";
 const Example = () => {
   return (
     <Layout>
@@ -11,15 +13,24 @@ const Example = () => {
       <Container>
         <div className={style.container}>
           <div className={style.article}>
-            <div className={style.header}></div>
-            <div className={style.details}></div>
-            <div className={style.heroImg}></div>
-            <div className={style.mainEvent}></div>
+            <div className={style.header}>Pulse selected for Kiln Startup Showcase</div>
+            <div className={style.details}>
+            <img className={style.authorImg} src={blake} /> January 17th 2020
+            </div>
+            <div className={style.heroImg}>
+            <img src={showcase} />
+            </div>
+            <div className={style.mainEvent}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
           </div>
           <div className={style.featured}>
+          <h2 className={style.featuredHeader}>Featured</h2>
             <div className={style.featuredItem}>
               <img className={style.featuredImg} src={logo} />
-              <div className={style.featuredText}></div>
+              <div className={style.featuredText}>
+              For Good: How the UX process helped us do something positive</div>
             </div>
           </div>
         </div>
