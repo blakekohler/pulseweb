5413578b14fb34c62cba284bf2245cc6700196d2
Blake Kohler <blakekohler@Blakes-MacBook-Pro.local>
2020-06-24 16:11:57 -0600

    added new image for product seo

--- src/pages/product.js
+++ src/pages/product.js
@@ -3,11 +3,11 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Container from "../components/contentContainer"
 import Features from "../components/product-components/features";
-
+import dashboardsmall from "../images/dashboardsmall.jpg"
 
 const Product = () => (
   <Layout>
-    <SEO title="Product" description="Learn more about the Pulse For Good product" />
+    <SEO title="Product" description="Learn more about the Pulse For Good product" img={dashboardsmall}/>
     <Container> <Features></Features></Container>
    
   </Layout>
